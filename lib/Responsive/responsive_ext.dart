d754c5c91417343be5b070181abf6f771f59ff58
Sanjay Sharma <sanjay.sharma@cerisetechsolutions.com>
2026-02-16 12:08:43 +0530

    manual issue solved

--- lib/Responsive/responsive_ext.dart
+++ lib/Responsive/responsive_ext.dart
@@ -5,7 +5,7 @@ import 'package:flutter/widgets.dart';
 
 extension ResponsiveExt on BoxConstraints {
   bool get isTablet => maxWidth >= 600;
-  double get contentMaxWidth => isTablet ? maxWidth : double.infinity;
+  double get contentMaxWidth => isTablet ? 540 : double.infinity;
   double get horizontalPadding => isTablet ? 30 : 15;
   double get contentMaxHeight => isTablet ? 70 : 60;
 
