9020ed73e926017860d338daffa78d46e05268e2
Sanjay Sharma <sanjay.sharma@cerisetechsolutions.com>
2026-02-17 16:39:56 +0530

    ip config done

--- lib/utilities/image_data.dart
+++ lib/utilities/image_data.dart
@@ -74,7 +74,7 @@ List<ProfileModel> profileGridValues = [
   const ProfileModel(2, 'Privacy Policy','Learn how we protect your data',privacyIcon,ProfileTrailingType.arrow),
   const ProfileModel(3, 'Terms & Conditions','Our rules, explained simply',termsIcon,ProfileTrailingType.arrow),
   const ProfileModel(4, 'Contact Us','We are here, if you need any help',contactUsIcon,ProfileTrailingType.arrow),
-  const ProfileModel(5, 'Ip Config','Sign out safely and easily',logoutIcon,ProfileTrailingType.none),
+  const ProfileModel(5, 'Ip Config','Change Ip Address to access',logoutIcon,ProfileTrailingType.none),
   const ProfileModel(6, 'Logout','Sign out safely and easily',logoutIcon,ProfileTrailingType.none),
   const ProfileModel(7, '','',versionControlIcon,ProfileTrailingType.none),
 ];
